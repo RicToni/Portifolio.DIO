37035d94ade99fe90eebea361ac8d46ce59a02e2
Ricardo Tonholli <ricardotonholli@gmail.com>
2023-10-19 02:44:02 -0300

    projeto base com css, html e JS pronto

---
+++ assets/JS/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const abas = document.querySelectorAll('.aba');
+    const conteudos = document.querySelectorAll('.aba-conteudo');
+  
+    abas.forEach(function (aba, index) {
+      aba.addEventListener('click', function () {
+        if (conteudos[index].style.display === 'block') {
+          conteudos[index].style.display = 'none';
+         
+        } else {
+          conteudos[index].style.display = 'block';
+        }
+      });
+    });
+  
+    // Adiciona um evento de clique aos botões "fechar"
+    const botoesFechar = document.querySelectorAll('.fechar');
+    botoesFechar.forEach(function (botao, index) {
+      botao.addEventListener('click', function () {
+        conteudos[index].style.display = 'none';
+      });
+    });
+  });
